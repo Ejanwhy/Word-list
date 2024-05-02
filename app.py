a2053f84c751d8c8e5777b52ba90485bfe5f9c16
Ejanwhy <120233305+Ejanwhy@users.noreply.github.com>
2024-05-02 08:28:38 +0800

    Update

--- app.py
+++ app.py
@@ -4,10 +4,20 @@ import requests
 from datetime import datetime
 from bson import ObjectId
 
-app = Flask(__name__)
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
-client = MongoClient('mongodb+srv://test:sparta@ferryza.h0wlcmc.mongodb.net/?retryWrites=true&w=majority&appName=Ferryza')
-db = client.dbsparta_plus_week2
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+app = Flask(__name__)
 
 @app.route('/')
 def main():
